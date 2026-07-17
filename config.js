6d431b4620c3536f34dd0631f74be2c4fc2bd5c9
Sagar Arora <sagar.arora@innovapptive.com>
2026-07-17 22:28:44 +0530

    new event changes

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
   API_BASE: "https://ticketbookingbackend-k2m9.onrender.com",
 
   // Event details
-  EVENT_ID: "YWP",
-  EVENT_NAME: "Yoga Wtih Paws ",
-  EVENT_DATE: "21st June 2026",
+  EVENT_ID: "KTP",
+  EVENT_NAME: "Kitty Party",
+  EVENT_DATE: "26th July 2026",
   EVENT_VENUE: "Jammin Studio, Bhopal",
 
   // Ticket price in INR
